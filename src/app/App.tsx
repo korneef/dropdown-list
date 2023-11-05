cbeb4dfd1ed588756fa952e5698d1ec681441f32
Valentin Korneev <v.korneef@gmail.com>
2023-11-05 14:37:47 +0600

    possibility to change plaseholder and name was added

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -7,10 +7,13 @@ import { carModelsData } from '../mockData/cars/cars';
 
 function App() {
   const [multiselect, setMultiselect] = useState(true);
-  const [expandable, setExpandable] = useState(true)
+  const [expandable, setExpandable] = useState(true);
   const [data, setData] = useState<Array<TData>>(languagesData)
   const [selectedOption, setSelectedOption] = useState('languages');
-  const [dropdownWidth, setDropdownWidth] = useState(400)
+  const [dropdownWidth, setDropdownWidth] = useState(400);
+  const [headerData, setHeaderData] = useState({
+    name: 'Языки', placeholder: 'Выберите язык'
+  })
 
   const handleOptionChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     console.log(selectedOption === 'cars')
@@ -18,9 +21,11 @@ function App() {
     switch (event.target.value) {
       case 'languages':
         setData(languagesData);
+        setHeaderData({name: 'Языки', placeholder: 'Выберите язык'})
         break
       case 'cars':
         setData(carModelsData);
+        setHeaderData({name: 'Автомобили', placeholder: 'Выберите автомобиль'})
         break
     }
   };
@@ -53,15 +58,15 @@ function App() {
         </div>
         <div>
           <label><input
-          type={ 'checkbox' }
-          checked={ multiselect }
-          onChange={ () => {
-            setData(prevState => prevState.map(item => {
-              return {...item, itemSelected: false}
-            }))
-            setMultiselect(!multiselect)
-          } }
-        /> Мультиселект</label>
+            type={ 'checkbox' }
+            checked={ multiselect }
+            onChange={ () => {
+              setData(prevState => prevState.map(item => {
+                return {...item, itemSelected: false}
+              }))
+              setMultiselect(!multiselect)
+            } }
+          /> Мультиселект</label>
         </div>
         <div><label><input
           type={ 'checkbox' }
@@ -83,10 +88,10 @@ function App() {
         <Dropdown
           data={ data }
           handleSelect={ setData }
-          name={ 'Языки' }
-          placeholder={ 'Выберите язык' }
+          name={ headerData.name }
+          placeholder={ headerData.placeholder }
           multiSelect={ multiselect }
-          expandable={expandable}
+          expandable={ expandable }
         />
       </div>
 
