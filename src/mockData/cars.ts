6167969ff9dc320c6d8034cbf2df182c8f37227b
Valentin Korneev <v.korneef@gmail.com>
2023-11-05 14:05:10 +0600

    fonts were added to dropdown module

---
+++ src/mockData/cars.ts
@@ -0,0 +1,17 @@
+export const carModelsData = [
+  { itemName: 'Toyota Corolla', itemSelected: false },
+  { itemName: 'Honda Civic', itemSelected: false },
+  { itemName: 'Ford Focus', itemSelected: false },
+  { itemName: 'Chevrolet Malibu', itemSelected: false },
+  { itemName: 'Nissan Altima', itemSelected: false },
+  { itemName: 'Volkswagen Jetta', itemSelected: false },
+  { itemName: 'Hyundai Elantra', itemSelected: false },
+  { itemName: 'Kia Forte', itemSelected: false },
+  { itemName: 'Mazda3', itemSelected: false },
+  { itemName: 'Subaru Impreza', itemSelected: false },
+  { itemName: 'BMW 3 Series', itemSelected: false },
+  { itemName: 'Mercedes-Benz C-Class', itemSelected: false },
+  { itemName: 'Audi A4', itemSelected: false },
+  { itemName: 'Lexus ES', itemSelected: false },
+  { itemName: 'Tesla Model 3', itemSelected: false }
+];
